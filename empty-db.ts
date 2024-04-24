a772b13583ec8c293db3e05488f9860242920741
endulum <71853916+endulum@users.noreply.github.com>
2024-04-24 15:03:36 -0700

    shorten db reset script and clarify its purpose

--- empty-db.ts
+++ empty-db.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
-import jsonwebtoken from 'jsonwebtoken'
 import User from './models/user'
 import 'dotenv/config'
+
+// empties prod database
 
 const uri: string | undefined = process.env.CONNECTION
 
@@ -13,18 +14,6 @@ async function main (): Promise<void> {
 
     await User.deleteMany({})
     console.log('Deleted all users.')
-
-    const user = await User.create({
-      username: 'demo-user-1',
-      password: 'password'
-    })
-    console.log('Created demo user.')
-
-    const token = jsonwebtoken.sign(
-      { username: user.username, id: user.id },
-      'secret' // MAKE THIS ACTUALLY SECRET
-    )
-    console.log(`Demo user token is ${token}`)
 
     console.log('Nothing left to do, closing connection.')
     void mongoose.connection.close()
